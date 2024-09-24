879323c9eefd3689c3e3437dd4919f058cd7410a
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-09-24 10:47:51 +0100

    Create reusable form field component

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,93 @@
+"use client";
+import React from "react";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+import Image from "next/image";
+
+interface CustomFormProps {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: any;
+  iconAlt?: any;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+}
+
+const RenderField = ({
+  field,
+  props,
+}: {
+  field: any;
+  props: CustomFormProps;
+}) => {
+  const { fieldType, iconSrc, iconAlt, placeholder } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500">
+          {props.iconSrc && (
+            <Image
+              src={iconSrc}
+              alt={iconAlt}
+              height={24}
+              width={24}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+
+      break;
+
+    default:
+      break;
+  }
+  //   return <Input type="text" placeholder="John Doe" />;
+};
+
+const CustomFormField = (props: CustomFormProps) => {
+  const { control, name, fieldType, label } = props;
+  return (
+    <>
+      <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <FormItem className="flex-1">
+            {fieldType !== FormFieldType.CHECKBOX && label && (
+              <FormLabel>{label}</FormLabel>
+            )}
+
+            <RenderField field={field} props={props} />
+
+            <FormMessage className="shad-error" />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+};
+
+export default CustomFormField;
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import CustomFormField from "../CustomFormField";
+
+export enum FormFieldType {
+  INPUT = "input",
+  CHECKBOX = "checkbox",
+  TEXTAREA = "textarea",
+  SELECT = "select",
+  PHONE_INPUT = "phoneInput",
+  DATE_PICKER = "datePicker",
+  SKELETON = "skeleton",
+}
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2)
+    .max(50),
+});
+
+const PatientForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there👋</h1>
+          <p className="text-dark-700">Schedule your first appointment</p>
+        </section>
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default PatientForm;
